4d64acd8ff4fe1bc76d3f7db490f5566afac9618
David Hamilton <nova706@gmail.com>
2023-04-11 08:57:30 -0600

    Use hook to build plugin-worker-thread

--- forge.config.ts
+++ forge.config.ts
@@ -5,6 +5,7 @@ import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { PublisherGithub } from '@electron-forge/publisher-github'
+import { execSync } from 'child_process';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -45,6 +46,11 @@ const config: ForgeConfig = {
             },
         }),
     ],
+    hooks: {
+        generateAssets: async (platform, arch) => {
+            execSync('npm run tsc -- --project tsconfig-worker-thread.json');
+        }
+    }
 };
 
 export default config;
